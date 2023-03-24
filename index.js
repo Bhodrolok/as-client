1b66ac5a8d88a2c0c8c729885bc186f2d028b9e4
Bennett Gunson <bengunson@gmail.com>
2023-03-24 17:52:31 -0600

    add img error handling for proxy replicas

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
+class ProxyReplica {
+    /**
+     * Circular linked list for defined server URLs 
+     */
+    constructor() {
+        this._index = 0; 
+        this._backups = [
+            "http://aspxy1.bhodrolok.xyz",
+            "http://aspxy2.bhodrolok.xyz",
+            "http://aspxy3.bhodrolok.xyz/",
+            "http://aspxy4.bhodrolok.xyz",
+            "http://aspxy5.bhodrolok.xyz",
+            "https://amazing-limiter-378022.uw.r.appspot.com",
+        ]
+    }
+
+    get current() {
+        return this._backups[this._index];
+    }
+
+    next() {
+        let current = this._backups[this._index];
+        this._index = ++this._index % this._backups.length;
+        return current;
+    }
+}
+
+const proxy = new ProxyReplica();
+
 document.querySelectorAll('.ad').forEach((ad, i) => {
     console.log(ad)
     setTimeout(() => {
-        ad.setAttribute('src', `https://amazing-limiter-378022.uw.r.appspot.com/ad?t=${Date.now()}`);
+        ad.setAttribute('src', `${proxy.current}/ad?t=${Date.now()}`);
+        ad.addEventListener('error', () => {
+            if (i === 0) {
+                // only advance to next replica once if this is the first ad
+                proxy.next();
+            }
+            ad.setAttribute('src', `${proxy.current}/ad?t=${Date.now()}`);
+        });
     }, 500*i);
 });
